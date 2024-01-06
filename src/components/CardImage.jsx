0fa53d2b16b6ed2e7efb43ea4eaa2d9c6c311604
Muhammad Yusuf <97136620+Yusufparker@users.noreply.github.com>
2024-01-06 21:07:14 +0700

    Update CardImage.jsx

--- src/components/CardImage.jsx
+++ src/components/CardImage.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function CardImage({imgUrl}) {
     return (
         <div className="mt-3 img-card">
-            <img src={imgUrl} alt="" className="img-fluid" />
+            <img src={imgUrl} alt="" className="img-fluid rounded" />
         </div>
     );
 }
